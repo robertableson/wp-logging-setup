bd23bdfbd40c8e8dd2aabcda302a2aa2a2abc836
Robert Ableson <robertableson@gmail.com>
2022-07-12 21:53:34 -0400

    Added info comment

--- wp-logging-setup.php
+++ wp-logging-setup.php
@@ -1,4 +1,15 @@
 <?php
+
+/*
+ * WP DEBUGGING
+ * To activate debugging, add the following in wp-config.php:
+ *  define( 'WP_DEBUG', true );
+ *  define( 'WP_DEBUG_DISPLAY', false );
+ *	define( 'WP_DEBUG_LOG', true );
+ * To debug simply, SFTP into website and check log file 
+ *  /logs/apache_wordpress-619001-2715337.cloudwaysapps.com.error.log
+ * */
+
 if ( ! function_exists('write_log')) {
     function write_log ( $log )  {
        if ( is_array( $log ) || is_object( $log ) ) {
